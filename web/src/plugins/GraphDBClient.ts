5367152240a61a0bc0a1e1aad97e9f14a8080508
Ashley <merienne.ashley@gmail.com>
2025-11-16 12:19:37 +0100

    feat: update default endpoint and include default prefixes in SPARQL query body

--- web/src/plugins/GraphDBClient.ts
+++ web/src/plugins/GraphDBClient.ts
@@ -1,9 +1,26 @@
 class GraphDBClient {
   // Static constants
   static readonly TIMEOUT                     = 300000; // 300 seconds
-  static readonly DEFAULT_ENDPOINT            = 'http://localhost:7200/repositories/';
+  static readonly DEFAULT_ENDPOINT            = 'http://localhost:5000/proxy/repositories/';
   static readonly DEFAULT_REPOSITORY          = 'PolitiRDF';
   static readonly CONTENT_TYPE_SPARQL_QUERY   = 'application/sparql-query';
+
+  private static DEFAULT_PREFIXES: string = `
+PREFIX politi: <http://politiRDF.com/>
+
+PREFIX schema: <http://schema.org/>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX owl: <http://www.w3.org/2002/07/owl#>
+PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+
+PREFIX wd: <http://www.wikidata.org/entity/>
+PREFIX wdt: <http://www.wikidata.org/prop/direct/>
+PREFIX wikibase: <http://wikiba.se/ontology#>
+PREFIX p: <http://www.wikidata.org/prop/>
+PREFIX ps: <http://www.wikidata.org/prop/statement/>
+PREFIX pq: <http://www.wikidata.org/prop/qualifier/>
+PREFIX bd: <http://www.bigdata.com/rdf#>
+`;
 
   // Static configuration
   private static endpoint:    string = GraphDBClient.DEFAULT_ENDPOINT;
@@ -34,12 +51,9 @@ class GraphDBClient {
     
     const response = await fetch(url, {
       method: 'POST',
-      headers: {
-        'Content-Type': GraphDBClient.CONTENT_TYPE_SPARQL_QUERY,
-        'Access-Control-Allow-Origin': '*',
-      },
-      body: sparqlQuery,
+      body: `${GraphDBClient.DEFAULT_PREFIXES}\n${sparqlQuery}`,
       signal: AbortSignal.timeout(GraphDBClient.TIMEOUT),
+      headers: { 'Content-Type': GraphDBClient.CONTENT_TYPE_SPARQL_QUERY, },
     });
 
     if (!response.ok) {
